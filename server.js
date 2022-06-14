e9029279cd460e612df3a58dc49ee5657519e683
Juan <juan@student.umn.ac.id>
2022-06-14 17:09:58 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require(`path`);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/profile.html'));
+});
+app.use(express.static('public'));
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
